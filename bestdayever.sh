3403ea47a6026ef02b262156dfcb16335db5fa36
Nathan-88 <onwukaebuka88@gmail.com>
2023-04-26 10:06:02 +0100

    This script greets the user with a nice message

--- bestdayever.sh
+++ bestdayever.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # This script greets the user with a nice message
+# and asks for their name.
+# If the user enters a name, the script greets them by name.
+# If the user does not enter a name, the script asks them to enter a name.
+user=$(whoami)
+date=$(date)
+whereami=$(pwd)
 if [ -n "$1" ];then
     name=$1
     echo "Good morning $name!"
@@ -7,6 +13,8 @@ if [ -n "$1" ];then
     echo "you are looking good today ${name}!"
     sleep 1
     echo "you have the cutest face i've ever seen $name"
+    sleep 2
+    echo "you are currently logged in as user: $user, you are in the directory: $whereami. today's is $date"
 else
     read -p "enter your name: " name
     echo "Good morning $name!"
@@ -14,4 +22,6 @@ else
     echo "you are looking good today ${name}!"
     sleep 1
     echo "you have the cutest face i've ever seen $name"
+    sleep 2
+    echo "you are currently logged in as user:$user, you are in the directory: $whereami. today's is $date"
 fi
