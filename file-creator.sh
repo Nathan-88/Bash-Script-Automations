977b9a5f685356c465739a889af35a0b4bcb0084
Nathan-88 <onwukaebuka88@gmail.com>
2023-04-26 09:52:47 +0100

    a script that creates a file and and inputs the header file into it

--- file-creator.sh
+++ file-creator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#a scripts that creates a file and opens with vim editor
+#a script that creates a file and and inputs the header file into it
 echo "input file name"
 read file_name
 touch $file_name
@@ -7,16 +7,31 @@ if [ "${file_name: -3}" == ".py" ]
 then
 	echo "#!/usr/bin/python3" > $file_name
 	chmod +x $file_name
-	vi $file_name
 elif [ "${file_name: -2}" == ".c" ]
 then
 	echo "#include <stdio.h>" > $file_name
-	vi $file_name
 elif [ "${file_name: -3}" == ".sh" ]
 then
 	echo "#!/bin/bash" > $file_name
 	chmod +x $file_name
-	vi $file_name
+elif [ "${file_name: -3}" == ".js" ]
+then
+	echo "#!/usr/bin/node" > $file_name
+	chmod +x $file_name
+elif [ "${file_name: -4}" == ".cpp" ]
+then
+	echo "#include <iostream>" > $file_name
+	echo "using namespace std;" >> $file_name
+	echo "int main(){" >> $file_name
+	echo "return 0;" >> $file_name
+	echo "}" >> $file_name
+elif [ "${file_name: -4}" == ".php" ]
+then
+	echo "<?php" > $file_name
+	echo "?>" >> $file_name
+elif [ "${file_name: -4}" == ".txt" ]
+then
+	echo " " > $file_name
 else
-	vi $file_name
+	echo "$file_name created successfully"
 fi
