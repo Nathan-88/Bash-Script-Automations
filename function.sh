acaaafa4070dc41bb53c13897deab57e3787a466
Nathan-88 <onwukaebuka88@gmail.com>
2023-05-01 13:32:55 +0100

    functions in bash

--- function.sh
+++ function.sh
@@ -16,17 +16,17 @@ function add {
 }
 
 # function call
-result=$(add 1 2)
+result=$(add 1 4)
 echo $result
 
 # function with multiple return values
 function add2 {
     local sum=$(( $1 + $2 ))
     local diff=$(( $1 - $2 ))
-    echo $sum
+    echo "$sum"
     echo $diff
 }
 
 # function call
-result=$(add2 1 2)
+result=$(add2 7 5)
 echo $result
